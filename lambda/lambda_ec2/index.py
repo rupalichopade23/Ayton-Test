8d1127ce747347bf5da92835a9102cba33da16e3
rupalichopade23 <rupali.chopade@sentia.com>
2023-01-15 19:57:43 +0100

    index file changed1

--- lambda/lambda_ec2/index.py
+++ lambda/lambda_ec2/index.py
@@ -5,7 +5,7 @@ import os
 def put_parameter():
     ssm_client = boto3.client('ssm')
     parameter_name = os.getenv("PARAMETER_NAME")
-    ssm_client.put_parameter(Name=parameter_name, Value="parameter_value", Type="String")
+    ssm_client.put_parameter(Name=os.getenv("PARAMETER_NAME"), Value="parameter_value", Type="String")
 
 def lambda_handler(event, context):
     put_parameter()
